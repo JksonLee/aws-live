5df05d08863d2a36408757b785286844e1e69483
JksonLee <130548331+JksonLee@users.noreply.github.com>
2023-04-12 16:39:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.crbm3puwdnbm.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leechisheng-employee"
+customregion = "us-east-1"
